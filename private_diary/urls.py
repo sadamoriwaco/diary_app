5e783f11239e447a9b020c22af7a03c5410bdb27
sadamoriwaco <97593024+sadamoriwaco@users.noreply.github.com>
2022-11-30 12:56:58 +0900

    LOGIN手前まで完了

--- private_diary/urls.py
+++ private_diary/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('diary.urls')),
     path('accounts/',include('allauth.urls')),
 ]
